e76e3f8f28962c1a58ac685982a2769cacdf686c
Abraão Lincoln Cunha de Sales <abraaolincolncunhadesales@gmail.com>
2025-06-26 17:27:46 +0000

    Extra Luck Number

--- src/components/e2/LuckyNumber.jsx
+++ src/components/e2/LuckyNumber.jsx
@@ -2,42 +2,41 @@ import { useState } from "react";
 import styles from "./LuckyNumber.module.css";
 
 export function LuckyNumber() {
-  const [luckyNumber, setLuckyNumber] = useState(1);
+  const [luckyNumber, setLuckyNumber] = useState(0);
   const [array, setArray] = useState([]);
 
   function handleClick() {
-    setLuckyNumber(Math.floor(Math.random() * 40) + 1);
-
-    {array.includes(luckyNumber) ? (
-          console.log("Lucky number already exists in the array")
-          
-        ) : (
-          setArray([...array, luckyNumber])
-        )}
-
+    const n = Math.floor(Math.random() * 40) + 1;
+    if (array.includes(n)) {
+      alert(`The number ${n} already exists in the array.`);
+      return;
+    }
+    setLuckyNumber(n);
+    setArray([...array, n]);
   }
-  const formatedArray = array.join(", ");
+
+  function sort() {
+    setArray([...array].sort((a, b) => a - b));
+  }
+
   return (
     <div className={styles.container}>
-        {luckyNumber === 0 ? (
-          <h1>Lucky Number 🎲</h1>
-        ) : (
-          <h1>Lucky Number = {luckyNumber}</h1>
-        )}
+      {luckyNumber === 0 ? (
+        <h1>Lucky Number 🎲</h1>
+      ) : (
+        <h1>Lucky Number = {luckyNumber}</h1>
+      )}
 
-        {array.includes(luckyNumber) ? (
-          <h1> O numero já foi sorteado</h1>
-          
-        ) : (
-          console.log("Lucky number is unique")
-        )}
-
-        
       <button className={styles.button} onClick={handleClick}>
         I'm Feeling Lucky Today!
       </button>
-      <h2>Números sorteados:</h2>
-      <h2>{formatedArray}</h2>
+      <div className={styles.sortedNumbers}>
+        <h2>Números sorteados:</h2>
+        <h2 className={styles.array}>{array.join(", ")}</h2>
+      </div>
+      <button className={styles.button} onClick={sort}>
+        Sort Numbers Alphabetically?
+      </button>
     </div>
   );
 }
